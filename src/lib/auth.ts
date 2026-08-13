32800d4771a8252379202440cbb0261e7821fe59
tanvir <mstanvirislam05@gmail.com>
2026-08-13 11:14:52 +0600

    feat: implement authentication setup with better-auth and create auth client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,15 +4,21 @@ import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
 });
 
 const prisma = new PrismaClient({
-  adapter,
+    adapter,
 });
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql",
-  }),
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+
+    emailAndPassword: {
+        enabled: true,
+    },
+
+
 });
